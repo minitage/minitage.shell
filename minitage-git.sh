d8f7b7efb9ebb03e32641c178c738ae66e6bbd61
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-05-31 15:31:02 +0200

    port to macosx tools

--- minitage-git.sh
+++ minitage-git.sh
@@ -1,29 +1,45 @@
 #!/usr/bin/env bash
 WHERE="$1"
+if [[ -z $WHERE ]];then
+    WHERE=$PWD
+fi
 cd $WHERE
+if [[ $(uname) == "Darwin" ]];then
+    SED="sed -E"
+else
+    SED="sed -re"
+fi
 WHERE="$PWD"
 PACKAGE="$(basename $WHERE)"
-BASE_URL="https://kiorky@github.com/minitage-dependencies"
+HBASE_URL="https://kiorky@github.com/minitage-dependencies"
 BASE_URL="git@github.com:minitage-dependencies"
-EGGS_URL="https://kiorky@github.com/minitage-eggs"
+HEGGS_URL="https://kiorky@github.com/minitage-eggs"
 EGGS_URL="git@github.com:minitage-eggs"
-MINILAYS_URL="https://kiorky@github.com/minitage/minilays.2.0."
+HMINILAYS_URL="https://kiorky@github.com/minitage/minilays.2.0."
 MINILAYS_URL="git@github.com:minitage/minilays.2.0."
 URL="$BASE_URL/$PACKAGE"
-if [[ $(echo $WHERE|sed -re "s/.*minilays.*/go/g") == "go" ]];then
+HURL="$HBASE_URL/$PACKAGE"
+if [[ $(echo $WHERE|$SED "s/.*minilays.*/go/g") == "go" ]];then
     URL="$MINILAYS_URL$PACKAGE"
+    HURL="$HMINILAYS_URL$PACKAGE"
 fi
-if [[ $(echo $WHERE|sed -re "s/.*eggs.*/go/g") == "go" ]];then
-        URL="$EGGS_URL/$PACKAGE"
+if [[ $(echo $WHERE|$SED "s/.*eggs.*/go/g") == "go" ]];then
+    URL="$EGGS_URL/$PACKAGE"
+    HURL="$HEGGS_URL/$PACKAGE"
 fi
 echo "Using $URL";read
 rm -rf .git
 git init
 git config github.user kiorky
-#git config github.token ef97c740b8e43f0c8fcc339584e9d9c9
 git config user.email  kiorky@cryptelium.net
 git config user.name  kiorky
 git config branch.master.merge refs/heads/master
 git config branch.master.remote origin
 git remote add origin $URL.git
-git pull -f
+git remote add h $HURL.git
+echo git remote add h $HURL.git
+httppull() {
+    git fetch h
+    git reset --hard remotes/h/master
+}
+git pull -f || httppull
